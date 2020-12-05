84fde0cf2dc0cda532072b60209b52aa7c422ef9
jorgeperezL <ovejo_jorge@hotmail.com>
2020-12-05 21:54:31 +0100

    Final, modificacion de funcion comparador, anadadido funcion resumen y comentarios

--- CACHEsym/src/CACHEsym.c
+++ CACHEsym/src/CACHEsym.c
@@ -12,32 +12,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BIT_0 0
+#define BIT_1 1
+
 typedef struct {
 	short int ETQ;
 	short int Datos[8];
 }T_LINEA_CACHE;
 
 
-int converBinDec(char * bin);
-char * converHexBin(char * hex);
+//Prototipos
+
 void leerTxt();
 void inicializarVariables();
 void leerBin();
-char * converHexBin(char * hex);
+void resumen();
+void comparador(unsigned int addr);
 
-int tiempoglobal = 0;
-int numfallos = 0;
+//Variables globales
+
+int tiempoglobal = BIT_0;
+int numfallos = BIT_0;
 T_LINEA_CACHE array[4];
 unsigned char RAM[1024];
+char texto[100];
+int num_addr = BIT_0;
 
 int main(int argc, char * argv[])
 {
 	inicializarVariables();
 	leerBin();
 	leerTxt();
+	resumen();
 
 	return 0;
 };
+
+//Salida del resumen de los datos de cache
+
+void resumen(){
+	printf("\n");
+	printf("Resumen:\n");
+	printf("=======\n");
+	printf("Numero de accesos: %d\n", num_addr);
+	printf("Numero de fallos: %d\n", numfallos);
+	printf("Tiempo medio: %d\n", tiempoglobal/num_addr);
+	printf("Texto: ");
+	for(int i = 0 ; i < num_addr ; i++){
+	printf("%c ", texto[i]);
+	}
+}
+
+//incializar variables ETQ a FF y Datos a 0
 
 void inicializarVariables(){
 	for(int i = 0; i< 4 ; i++){
@@ -47,6 +73,8 @@ void inicializarVariables(){
 			}
 		}
 }
+
+//Parseo de fichero RAM.bin
 
 void leerBin(){
 		FILE *fp;
@@ -64,12 +92,14 @@ void leerBin(){
 		fclose (fp);
 }
 
+//Parseo de fichero acceso_memoria.txt
+
 void leerTxt(){
 
 			FILE *fp;
 
-		 	char buffer[10];
-		 	char num_2[30];
+
+		 	unsigned int addr;
 
 		 	fp = fopen ( "accesos_memoria.txt", "r" );
 
@@ -80,154 +110,60 @@ void leerTxt(){
 		 	}
 
 		 	while(!feof(fp)){
-		 	fscanf(fp, "%s" ,buffer);
-		 		char * bin = converHexBin(buffer);
-		 		comparador(bin);
-		 		printf("%s\n",bin);
-
-		 		//sleep(2);
+		 	fscanf(fp, "%X" ,&addr);
+		 		comparador(addr);
+		 		sleep(2);
 		 	}
 		 	fclose ( fp );
 }
 
-void comparador(char * bin){
-	char etq[5];
-	char linea[2];
-	char palabra[3];
-	char n_bloque[7];
+/*Descomposicion de addr en etiqueta, linea y palabra,
+comparador de etiqueta con linea cache y
+salida de resultados
+*/
+void comparador(unsigned int addr){
 
-	for(int i = 0 ; i < 5 ; i++){
-		etq[i] = bin[6+i];
-	}
+	unsigned int palabra = (addr & 0b0000000111);
+	unsigned int linea = (addr & 0b0000011000) >> 3;
+	unsigned int etiqueta = (addr & 0b1111100000) >> 5;
+	unsigned int n_bloque = (addr & 0b1111111000) >> 3;
 
-	for(int i = 0 ; i < 2 ; i++){
-		linea[i] = bin[11+i];
-	}
+	int pin_entrada = BIT_0;
 
-	for(int i = 0 ; i < 3 ; i++){
-		palabra[i] = bin[13+i];
-	}
-
-	for(int i = 0 ; i < 7 ; i++){
-		n_bloque[i]=bin[6+i];
-	}
-
-	int etq_dec = converBinDec(etq);
-	int linea_dec = converBinDec(linea);
-	int hex_dec = converBinDec(bin);
-	int palabra_dec = converBinDec(palabra);
-	int n_bloque_dec = converBinDec(n_bloque);
-
-
-	int pin_entrada = 0;
-
-	do {
-
-		if(array[linea_dec].ETQ == etq_dec){
-			pin_entrada = 1;
-			tiempoglobal = tiempoglobal +10;
-			int dato = array[linea_dec].Datos[palabra_dec];
-			printf("T: %d, Acierto de CACHE, ADDR %04X ETQ %X \
-					linea %02X palabra %02X DATO %02X",tiempoglobal,hex_dec,etq_dec,linea_dec,palabra_dec,dato);
-
-			for(int i = 0; i< 4 ; i++){
-						printf("ETQ: ");
-						printf("%d ",array[i].ETQ);
-						printf("Datos ");
-						for(int j = 0 ; j < 8 ; j++){
-							printf("%d ",array[i].Datos[j]);
+		do {
+			if(array[linea].ETQ == etiqueta){
+				pin_entrada = BIT_1;
+				tiempoglobal = tiempoglobal +10;
+				texto[num_addr] = RAM[addr];
+				num_addr = num_addr +1;
+				printf("T: %d, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X DATO %02X\n"
+						,tiempoglobal,addr,etiqueta,linea,palabra,array[linea].Datos[palabra]);
+				for(int i = 0; i< 4 ; i++){
+							printf("ETQ: ");
+							printf("%X ",array[i].ETQ);
+							printf("Datos ");
+							int temporal;
+							for (int x = 0; x < 8 / 2; x++) {
+								temporal = array[i].Datos[x];
+								array[i].Datos[x] = array[i].Datos[8 - x - 1];
+								array[i].Datos[8 - x - 1] = temporal;
+							}
+							for(int j = 0 ; j < 8 ; j++){
+								printf("%02X ",array[i].Datos[j]);
+							}
+							printf("\n");
 						}
-					}
+			}
+		if(pin_entrada == 0){
+			tiempoglobal = tiempoglobal +10;
+			numfallos = numfallos + 1;
+			printf("T: %d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X\n"
+					,tiempoglobal,numfallos,addr,etiqueta,linea,palabra,n_bloque);
+			printf("Cargando el bloque %02X en la linea %02X\n",n_bloque,linea);
+			array[linea].ETQ = etiqueta;
+			for(int i = 0 ; i < 8 ; i++){
+				array[linea].Datos[i] = RAM[n_bloque*8 + i];
+			}
 		}
-	if(pin_entrada == 0){
-		tiempoglobal = tiempoglobal +10;
-		numfallos = numfallos + 1;
-		printf("“T: %d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X \
-			palabra %02X bloque %02X",tiempoglobal,numfallos,hex_dec,etq_dec,linea_dec,palabra_dec,n_bloque_dec);
-		printf("“Cargando el bloque %02X en la linea %02X ",n_bloque_dec,linea_dec);
-		array[linea_dec].ETQ = etq_dec;
-		for(int i = 0 ; i < 8 ; i++){
-			array[linea_dec].Datos[i] = RAM[n_bloque_dec*8 + i];
-		}
-	}
-	} while(pin_entrada == 0);
-}
-
-int converBinDec(char * bin){
-	int dec = 0;
-	while (bin != '\n') {
-	if (bin == '1') dec = dec * 2 + 1;
-	else if (bin == '0') dec *= 2; }
-	return dec;
-}
-
-char * converHexBin(char * hex){
-
-	char * bin = (char*)malloc(17*sizeof(char));
-	bin[0] = '\0';
-	    for(int i=0; hex[i]!='\0'; i++)
-	    {
-
-	    	switch(hex[i])
-	        {
-	            case '0':
-	                strcat(bin, "0000");
-	                break;
-	            case '1':
-	                strcat(bin, "0001");
-	                break;
-	            case '2':
-	                strcat(bin, "0010");
-	                break;
-	            case '3':
-	                strcat(bin, "0011");
-	                break;
-	            case '4':
-	                strcat(bin, "0100");
-	                break;
-	            case '5':
-	                strcat(bin, "0101");
-	                break;
-	            case '6':
-	                strcat(bin, "0110");
-	                break;
-	            case '7':
-	                strcat(bin, "0111");
-	                break;
-	            case '8':
-	                strcat(bin, "1000");
-	                break;
-	            case '9':
-	                strcat(bin, "1001");
-	                break;
-	            case 'a':
-	            case 'A':
-	                strcat(bin, "1010");
-	                break;
-	            case 'b':
-	            case 'B':
-	                strcat(bin, "1011");
-	                break;
-	            case 'c':
-	            case 'C':
-	                strcat(bin, "1100");
-	                break;
-	            case 'd':
-	            case 'D':
-	                strcat(bin, "1101");
-	                break;
-	            case 'e':
-	            case 'E':
-	                strcat(bin, "1110");
-	                break;
-	            case 'f':
-	            case 'F':
-	                strcat(bin, "1111");
-	                break;
-	            default:
-	                printf("Error en hexadecimal");
-	        }
-	    }
-	    bin[16] = '\0';
-	    return bin;
+		} while(pin_entrada == 0);
 }
